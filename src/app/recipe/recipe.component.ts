6bf85aeae07f33accf42517bda3ac38ee6feb7fb
Julius-717 <waynejulius10@gmail.com>
2019-11-14 19:47:27 +0300

    make changes to recipe.components.ts

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { Recipe } from './recipe.model';
+import { AuthService } from './../../services/auth.service';
+import { RecipeService } from './../../services/recipe.service';
+import { Recipe } from './../recipe.model';
+import { Component, OnInit, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-recipe',
-  templateUrl: './recipe.component.html',
-  styleUrls: ['./recipe.component.css']
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeComponent implements OnInit {
-  recipes: Recipe[] = [
-  new Recipe("Test Recipe", "This is just a test description", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmChnck-LcjqL6Q1Eld6-JefHdN0rcLK3Sh1cdbWzxBv0z6Poqdg&s"),
-  new Recipe("Rib Eye", "This is just a test description", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmChnck-LcjqL6Q1Eld6-JefHdN0rcLK3Sh1cdbWzxBv0z6Poqdg&s")
-  ];
+export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+
+  recipes: Recipe[] = [];
+
+
+  constructor(public recipeService: RecipeService, public auth: AuthService) { }
 
   ngOnInit() {
+    this.recipeService.recipesChanged.subscribe(
+      (recipes: Recipe[]) => {
+        this.recipes = recipes
+      }
+    )
+    this.recipes = this.recipeService.getRecipes()
   }
 
 }
