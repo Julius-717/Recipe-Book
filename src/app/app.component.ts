b2c8e6c541125f368eaa5513d065b31aa2cef89d
Julius-717 <waynejulius10@gmail.com>
2019-11-14 19:39:32 +0300

    add all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
+import { AuthService } from './../services/auth.service';
+import { Response } from '@angular/http';
+import { DataStorageService } from './../services/data-storage.service';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
-export class AppComponent {
-  
+
+export class HeaderComponent {
+
+  constructor(public storage: DataStorageService, public auth: AuthService) { }
+
+  onSaveData() {
+    this.storage.storeRecipes()
+      .subscribe(
+      (response: Response) => console.log(response)
+      );
+  }
+
+  fetchData() {
+    this.storage.getRecipes();
+  }
+
+  onLogout() {
+    this.auth.onLogout();
+  }
+
+  toggleMenu(){
+    let div = document.querySelector('#toToggle');
+    div.classList.toggle('in');
+  }
+
 }
