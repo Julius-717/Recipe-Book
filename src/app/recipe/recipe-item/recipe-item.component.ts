2ce3d13e3d38a88a525fb3ab922f8f1dd662455c
Julius-717 <waynejulius10@gmail.com>
2019-11-14 19:50:25 +0300

    add all

--- src/app/recipe/recipe-item/recipe-item.component.ts
+++ src/app/recipe/recipe-item/recipe-item.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Recipe } from './../../recipe.model';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
 })
-export class RecipeItemComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+export class RecipeItemComponent {
+  @Input() recipe: Recipe;
+  @Input() i: number;
 
 }
