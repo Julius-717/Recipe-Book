097087a708b3f49d4377abb42bfabe3c729304ef
Julius-717 <waynejulius10@gmail.com>
2019-11-14 17:52:07 +0300

    add row class to shopping-list.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping_list/shopping-edit/shopping-edit.component';
 import { RecipeComponent } from './recipe/recipe.component';
 import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipe/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
     RecipeComponent,
     RecipeListComponent,
     RecipeItemComponent,
-    RecipeDetailComponent
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -8,9 +8,10 @@ import { Recipe } from './recipe.model';
 })
 export class RecipeComponent implements OnInit {
   recipes: Recipe[] = [
-  new Recipe("Test Recipe","This is just a test description", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmChnck-LcjqL6Q1Eld6-JefHdN0rcLK3Sh1cdbWzxBv0z6Poqdg&s")
+  new Recipe("Test Recipe", "This is just a test description", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmChnck-LcjqL6Q1Eld6-JefHdN0rcLK3Sh1cdbWzxBv0z6Poqdg&s"),
+  new Recipe("Rib Eye", "This is just a test description", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmChnck-LcjqL6Q1Eld6-JefHdN0rcLK3Sh1cdbWzxBv0z6Poqdg&s")
   ];
-  
+
   constructor() { }
 
   ngOnInit() {
